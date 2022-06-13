9f6ca3ea8020bc8057157c2d11c9ec479d0b216f
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2022-06-13 09:05:20 +0000

    fix dates, move rrules

--- node/rrule-gen.js
+++ node/rrule-gen.js
@@ -1,78 +1,78 @@
 import smu from "rrule";
 const { RRuleSet, RRule } = smu;
 
-// 3 days a week
-const rule1 = new RRule(
-  {
-    freq: RRule.WEEKLY,
-    dtstart: new Date(Date.UTC(2021, 2, 1, 10, 30)),
-    interval: 1,
-    byweekday: [RRule.MO, RRule.WE, RRule.FR],
-  },
-  true
-);
-// Daily
-const rule2 = new RRule(
-  {
-    freq: RRule.DAILY,
-    dtstart: new Date(Date.UTC(2021, 2, 1, 10, 30)),
-  },
-  true
-);
-
-// With exclusions and byweekday
-const rule3 = new RRuleSet(true);
-rule3.rrule(
-  new RRule({
-    freq: RRule.WEEKLY,
-    dtstart: new Date(Date.UTC(2021, 2, 1, 17, 30)),
-    interval: 1,
-  })
-);
-
-rule3.exdate(new Date(Date.UTC(2021, 8, 16, 17, 30)));
-rule3.exdate(new Date(Date.UTC(2021, 9, 27, 17, 30)));
-rule3.rdate(new Date(Date.UTC(2021, 8, 5, 10, 30)));
-rule3.rdate(new Date(Date.UTC(2021, 8, 16, 10, 30)));
-rule3.rdate(new Date(Date.UTC(2021, 9, 27, 10, 30)));
-
 export function generateEvents() {
+  // 3 days a week
+  const rule1 = new RRule(
+    {
+      freq: RRule.WEEKLY,
+      dtstart: new Date(Date.UTC(2021, 1, 1, 10, 30)),
+      interval: 1,
+      byweekday: [RRule.MO, RRule.WE, RRule.FR],
+    },
+    true
+  );
+  // Daily
+  const rule2 = new RRule(
+    {
+      freq: RRule.DAILY,
+      dtstart: new Date(Date.UTC(2021, 1, 1, 10, 30)),
+    },
+    true
+  );
+
+  // With exclusions and byweekday
+  const rule3 = new RRuleSet(true);
+  rule3.rrule(
+    new RRule({
+      freq: RRule.WEEKLY,
+      dtstart: new Date(Date.UTC(2021, 1, 1, 17, 30)),
+      interval: 1,
+    })
+  );
+
+  rule3.exdate(new Date(Date.UTC(2021, 7, 16, 17, 30)));
+  rule3.exdate(new Date(Date.UTC(2021, 8, 27, 17, 30)));
+  rule3.rdate(new Date(Date.UTC(2021, 7, 5, 10, 30)));
+  rule3.rdate(new Date(Date.UTC(2021, 7, 16, 10, 30)));
+  rule3.rdate(new Date(Date.UTC(2021, 8, 27, 10, 30)));
+
   return [
     ...rule1.between(
-      new Date(Date.UTC(2021, 8, 1)),
-      new Date(Date.UTC(2021, 8, 7, 23, 59, 59)),
+      new Date(Date.UTC(2021, 7, 1)),
+      new Date(Date.UTC(2021, 7, 7, 23, 59, 59)),
       true
     ),
     ...rule2.between(
-      new Date(Date.UTC(2021, 8, 1)),
-      new Date(Date.UTC(2021, 8, 7, 23, 59, 59)),
+      new Date(Date.UTC(2021, 7, 1)),
+      new Date(Date.UTC(2021, 7, 7, 23, 59, 59)),
       true
     ),
     ...rule3.between(
-      new Date(Date.UTC(2021, 8, 1)),
-      new Date(Date.UTC(2021, 8, 7, 23, 59, 59)),
+      new Date(Date.UTC(2021, 7, 1)),
+      new Date(Date.UTC(2021, 7, 7, 23, 59, 59)),
       true
     ),
   ];
 }
 
-function generateRandomDate(interval) {
-  const startDate = new Date(2002, 1, 1).valueOf();
-  const timestamp = Math.round(Math.random() * startDate);
-  return [new Date(timestamp), new Date(timestamp + interval)];
-}
+// function generateRandomDate(interval) {
+//   const startDate = new Date(2002, 1, 1).valueOf();
+//   const timestamp = Math.round(Math.random() * startDate);
+//   return [new Date(timestamp), new Date(timestamp + interval)];
+// }
 
-export function projectMaxExpectedEvents() {
-  const iterations = 1000000;
-  let res = [];
+// export function projectMaxExpectedEvents() {
+//   const iterations = 1000000;
+//   let res = [];
 
-  for (let i = 0; i < iterations; i++) {
-    const [min, max] = generateRandomDate(60 * 60);
-    res = [...res, rule1.between(min, max)];
-  }
+//   for (let i = 0; i < iterations; i++) {
+//     const [min, max] = generateRandomDate(60 * 60);
+//     res = [...res, rule1.between(min, max)];
+//   }
 
-  return res;
-}
+//   return res;
+// }
 
 const numberOfEvents = 10000;
 
@@ -173,7 +173,7 @@ export function projectMultipleRulesWeekly() {
 
 export function projectMultipleRulesWeeklyAsync() {
   console.log("A");
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     const dtstart = new Date(Date.UTC(2021, 1, 1, 10, 30));
     const rule = new RRule(
       {
